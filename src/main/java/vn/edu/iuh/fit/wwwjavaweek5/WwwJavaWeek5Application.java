3e21c6063bbd5fa8d9d607371745a9144ba11e65
Tran Quang Huy <107989088+tranquanghuy-09@users.noreply.github.com>
2023-10-15 06:11:20 +0700

    update v1.01

---
+++ src/main/java/vn/edu/iuh/fit/wwwjavaweek5/WwwJavaWeek5Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.wwwjavaweek5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwJavaWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwJavaWeek5Application.class, args);
+	}
+
+}
